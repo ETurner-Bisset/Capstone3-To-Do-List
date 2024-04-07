ebe5cc96ffb374dd8b278db3758ddc3de84d7dcd
ETurner-Bisset <elli_t29@hotmail.com>
2024-04-07 10:10:04 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import mongoose from "mongoose";
 import _ from "lodash";
 
 const app = express();
-const port = 3000;
+const port = 10000;
 
 const date = new Date();
 const options = {weekday: "long", year: "numeric", month: "short", day: "2-digit"};
